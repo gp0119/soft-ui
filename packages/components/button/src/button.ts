1bf6d65e8f15e9d9026d1fb1fa47c7916f81be87
ganpeng <190875565@qq.com>
2022-11-22 22:01:09 +0800

    wip: button

--- packages/components/button/src/button.ts
+++
@@ -1,3 +0,0 @@
-export const buttonProps = {
-  type: {},
-}
---
+++ packages/components/button/src/buttonTypes.ts
@@ -0,0 +1,4 @@
+export type ButtonType = 'default' | 'primary' | 'success' | 'warning' | 'danger' | 'info'
+export const buttonProps = {
+  type: {},
+}
